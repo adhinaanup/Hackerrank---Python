5324888f4b27ff0da0a9ddb2c425d0eec074d7ae
Adhina Anup <148629512+adhinaanup@users.noreply.github.com>
2025-06-15 23:45:52 +0530

    Update Second lowest grade.py

--- Second lowest grade.py
+++ Second lowest grade.py
@@ -1,18 +1,1 @@
-n=int(input("Enter the no of students : "))
-li=[]
-for i in range(n):
-  li.append([name,grade])
-li.sorted(key=lambda x:x[1])
-lowest=li[0][1]
-second=0.0
-for name,grade in li:
-  if grade>lowest:
-    second=grade
-    break
-result=[]
-for name,grade in li:
-  if grade==second:
-    result.append(name)
-result.sort()
-for i in result:
-  print(i)
+
